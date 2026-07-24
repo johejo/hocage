5b59cf416950fd19d4457d9e0a7f714ec41d8da7
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-07-24 18:06:52 +0900

    dedupe hocage sub-library list between CompileOptions and ProgramOptions

--- cellib.go
+++ cellib.go
@@ -8,38 +8,36 @@ func HocageLibrary() cel.EnvOption {
 	return cel.Lib(&hocageLib{})
 }
 
+func hocageSubLibraries() []cel.Library {
+	return []cel.Library{
+		&fsLib{},
+		&gitLib{},
+		&envLib{},
+		&globLib{},
+		&mapLib{},
+		&strLib{},
+		&shLib{},
+		&listLib{},
+		&semverLib{},
+		&encodingLib{},
+		&defaultLib{},
+		&cryptoLib{},
+		&transcriptLib{},
+	}
+}
+
 func (l *hocageLib) CompileOptions() []cel.EnvOption {
 	var opts []cel.EnvOption
-	opts = append(opts, (&fsLib{}).CompileOptions()...)
-	opts = append(opts, (&gitLib{}).CompileOptions()...)
-	opts = append(opts, (&envLib{}).CompileOptions()...)
-	opts = append(opts, (&globLib{}).CompileOptions()...)
-	opts = append(opts, (&mapLib{}).CompileOptions()...)
-	opts = append(opts, (&strLib{}).CompileOptions()...)
-	opts = append(opts, (&shLib{}).CompileOptions()...)
-	opts = append(opts, (&listLib{}).CompileOptions()...)
-	opts = append(opts, (&semverLib{}).CompileOptions()...)
-	opts = append(opts, (&encodingLib{}).CompileOptions()...)
-	opts = append(opts, (&defaultLib{}).CompileOptions()...)
-	opts = append(opts, (&cryptoLib{}).CompileOptions()...)
-	opts = append(opts, (&transcriptLib{}).CompileOptions()...)
+	for _, sub := range hocageSubLibraries() {
+		opts = append(opts, sub.CompileOptions()...)
+	}
 	return opts
 }
 
 func (l *hocageLib) ProgramOptions() []cel.ProgramOption {
 	var opts []cel.ProgramOption
-	opts = append(opts, (&fsLib{}).ProgramOptions()...)
-	opts = append(opts, (&gitLib{}).ProgramOptions()...)
-	opts = append(opts, (&envLib{}).ProgramOptions()...)
-	opts = append(opts, (&globLib{}).ProgramOptions()...)
-	opts = append(opts, (&mapLib{}).ProgramOptions()...)
-	opts = append(opts, (&strLib{}).ProgramOptions()...)
-	opts = append(opts, (&shLib{}).ProgramOptions()...)
-	opts = append(opts, (&listLib{}).ProgramOptions()...)
-	opts = append(opts, (&semverLib{}).ProgramOptions()...)
-	opts = append(opts, (&encodingLib{}).ProgramOptions()...)
-	opts = append(opts, (&defaultLib{}).ProgramOptions()...)
-	opts = append(opts, (&cryptoLib{}).ProgramOptions()...)
-	opts = append(opts, (&transcriptLib{}).ProgramOptions()...)
+	for _, sub := range hocageSubLibraries() {
+		opts = append(opts, sub.ProgramOptions()...)
+	}
 	return opts
 }
